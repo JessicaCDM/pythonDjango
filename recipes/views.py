2ed27bc446fb5588ca956b6863bd89977ad53acf
JessicaCDM <jessica.donzelli@gmail.com>
2023-08-08 16:08:28 +0100

    update geral

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from django.http import Http404
+
+from .models import Recipe
 
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(6)],
+        'recipes': recipes,
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': make_recipe(),
         'is_detail_page': True,
+    })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True,
+        ).order_by('-id')
+    
+    if not recipes:
+        raise Http404('Not found 😤')
+
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | '
     })
