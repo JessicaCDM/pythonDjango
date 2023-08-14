d24ad04affef5f7d028704d7d43a8a785d9f2201
JessicaCDM <jessica.donzelli@gmail.com>
2023-08-14 10:09:31 +0100

    update

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.http.response import Http404
+from django.db.models import Q
+from django.core.paginator import Paginator
+from utils.pagination import make_pagination_range
 
 from .models import Recipe
 
@@ -6,8 +10,24 @@ def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
+
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+    
+    paginator = Paginator(recipes, 9)
+    page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range
     })
 
 def recipe(request, id):
@@ -33,4 +53,24 @@ def category(request, category_id):
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         'title': f'{recipes[0].category.name} - Category | '
+    })
+
+def search(request):
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        raise Http404()
+    
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains = search_term) |
+            Q(description__icontains = search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
+    return render(request, 'recipes/pages/search.html', {
+        'page_title': f'Search for "{search_term}" |',
+        'search_term': search_term,
+        'recipes': recipes,
     })
